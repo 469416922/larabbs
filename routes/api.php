58d5cb8001ebfc4ca7b3e2b5f7c901ecf116fb83
李跃 <phliyue@hotmail.com>
2019-05-02 16:01:19 +0800

    修改路由

--- routes/api.php
+++ routes/api.php
@@ -3,14 +3,6 @@
 use Illuminate\Http\Request;
 
 $api = app('Dingo\Api\Routing\Router');
-
-$api->version('v1', [
-    'namespace' => 'App\Http\Controllers\Api'
-], function($api) {
-    // 短信验证码
-    $api->post('verificationCodes', 'VerificationCodesController@store')
-        ->name('api.verificationCodes.store');
-});
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api'
 ], function($api) {
